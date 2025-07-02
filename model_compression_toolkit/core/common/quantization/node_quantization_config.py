7b02b6d9a6f8ca1fd32455db8eb7e0ab6f285c21
gouda-youichi <gouda.youichi@jp.panasonic.com>
2025-07-02 15:42:44 +0900

    revert an assert in set_activation_quantization_param.

--- model_compression_toolkit/core/common/quantization/node_quantization_config.py
+++ model_compression_toolkit/core/common/quantization/node_quantization_config.py
@@ -143,17 +143,15 @@ class NodeActivationQuantizationConfig(BaseNodeQuantizationConfig):
         return self.quant_mode == ActivationQuantizationMode.FLN_QUANT
 
     def set_activation_quantization_param(self,
-                                          activation_params: dict,
-                                          node: BaseNode):
+                                          activation_params: dict):
         """
          Set a quantization parameter for the node's activation.
 
         Args:
             activation_params: Dictionary that contains weight quantization params.
-            node: node in a graph that represents the model.
 
         """
-        assert node.is_activation_quantization_enabled() or node.is_fln_quantization()
+        assert self.quant_mode == ActivationQuantizationMode.QUANT or self.quant_mode == ActivationQuantizationMode.FLN_QUANT
         for param_name, param_value in activation_params.items():
             self.activation_quantization_params[param_name] = param_value
 
